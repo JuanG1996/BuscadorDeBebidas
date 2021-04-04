9c2d10aa225d4a7470dcf2285893be0eb2ad9210
Juan Gaytan <arc_juan_1@hotmail.com>
2021-04-04 01:52:37 -0600

    app finalizada

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import Formulario from "./components/Formulario";
 import CategoriasProvider from "./context/CategoriasContext";
 import RecetasProvider from './context/RecetasContext';
 import ListaRecetas from './components/ListaRecetas';
+import ModalProvider from "./context/ModalContext";
 
 
 function App() {
   return (
     <CategoriasProvider>
       <RecetasProvider>
+        <ModalProvider>
 
         <Header />
+        
         <div className="container mt-5">
-          <div className="row">
-            <Formulario />
-          </div>
-          <ListaRecetas />
+
+            <div className="row">
+              <Formulario />
+            </div>
+
+            <ListaRecetas />
+
         </div>
 
+        </ModalProvider>
       </RecetasProvider>
     </CategoriasProvider>
   );
